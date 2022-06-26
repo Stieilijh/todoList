04d46c22754e9f7628df6f19690eebc21d757c32
Stieilijh <saitushar02@gmail.com>
2022-06-26 11:51:07 +0530

    add and remove project works

--- src/index.js
+++ src/index.js
@@ -3,23 +3,32 @@ import TodoList from "./assets/Todolist";
 import Project from "./assets/Project";
 
 //const TodoListDiv = document.querySelector("#todolist");
-const projectsDiv = document.querySelector("#projects");
 
 const todoList = new TodoList();
-function addProjectToDiv(name) {
-  const projectDiv = document.createElement("div");
-  projectDiv.id = name;
-  projectDiv.textContent = name;
-  projectDiv.classList = "projectList";
-  projectsDiv.appendChild(projectDiv);
-}
+
 function UpdateProjectNames() {
+  const projectsDiv = document.querySelector("#projects");
+  projectsDiv.innerHTML = "";
   todoList.getprojects().forEach((element) => {
     const projectDiv = document.createElement("div");
     projectDiv.id = element.getName();
     projectDiv.textContent = element.getName();
     projectDiv.classList = "projectList";
     projectsDiv.appendChild(projectDiv);
+    //styling the new project
+    projectDiv.style.display = "flex";
+    projectDiv.style.justifyContent = "space-around";
+    projectDiv.style.alignItems = "center";
+    //delete project btn
+    const delBtn = document.createElement("button");
+    delBtn.id = element.getName() + "DelBtn";
+    delBtn.textContent = "x";
+    delBtn.classList = "cancelBtns";
+    delBtn.addEventListener("click", () => {
+      todoList.deleteproject(element.getName());
+      UpdateProjectNames();
+    });
+    projectDiv.appendChild(delBtn);
   });
 }
 function MakeaddProjectFormInvisible() {
@@ -31,23 +40,29 @@ window.onload = () => {
   MakeaddProjectFormInvisible();
   UpdateProjectNames();
 
-  //add project button ActionListner
+  //cancel new project
+  document.querySelector("#cancelNewProject").addEventListener("click", () => {
+    MakeaddProjectFormInvisible();
+  });
+
+  //add project button ActionListener
   document.querySelector("#addProjectBtn").addEventListener("click", () => {
     addProjectBtn.style.display = "none";
     document.querySelector("#addProjectFormDiv").style.display = "block";
   });
   //addProjectform submit
   document
-    .querySelector("#addProjectForm")
-    .addEventListener("submit", (event) => {
-      event.preventDefault();
+    .querySelector("#createNewProjectBtn")
+    .addEventListener("click", () => {
       const name = document.querySelector("#addProjectNameInput").value;
       if (todoList.has(name)) {
         alert("Project already exists");
+      } else if (name === "") {
+        alert("Name can't be empty");
       } else {
         MakeaddProjectFormInvisible();
         todoList.addproject(new Project(name));
-        addProjectToDiv(name);
+        UpdateProjectNames();
       }
     });
 };
